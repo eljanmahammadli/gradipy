f030cdaa086214d2398d8290ea33bd8680c87676
Eljan Mahammadli <eljanmahammadli@gmail.com>
2023-11-02 13:26:28 -0400

    added: matmul, add, relu, zero_grad, backward

--- gradipy/tensor.py
+++ gradipy/tensor.py
@@ -2,24 +2,52 @@ import numpy as np
 
 
 class Tensor:
-    def __init__(self, data, requires_grad=False):
+    def __init__(self, data, _children=()):
         self.data = (
             data if isinstance(data, np.ndarray) else np.array(data, dtype=np.float32)
         )
         # ones_like is for test purposes only
-        self._requires_grad = requires_grad
-        self.grad = np.ones_like(self.data) if requires_grad else None
+        self.grad = np.zeros_like(self.data, dtype=np.float32)
         self._backward = lambda: None
+        self._prev = set(_children)
 
     def __repr__(self):
         return f"Tensor({self.data})"
+
+    def zero_grad(self):
+        self.grad = np.zeros_like(self.data, dtype=np.float32)
 
     @property
     def shape(self):
         return self.data.shape
 
+    def reshape(self, *shape):
+        return Tensor(self.data.reshape(*shape))
+
+    def _broadcast(self, other):
+        bx, by = np.broadcast_arrays(self.data, other.data)
+        ax = tuple(
+            i
+            for i, (dx, dy) in enumerate(zip(bx.strides, by.strides))
+            if dx == 0 and dy != 0
+        )
+        ay = tuple(
+            i
+            for i, (dx, dy) in enumerate(zip(bx.strides, by.strides))
+            if dx != 0 and dy == 0
+        )
+        return ax, ay
+
     def __add__(self, other):
-        return Tensor(self.data + other.data)
+        out = Tensor(self.data + other.data, _children=(self, other))
+
+        def _backward():
+            ax, ay = self._broadcast(other)
+            self.grad += 1.0 * np.sum(out.grad, ax).reshape(self.shape)
+            other.grad += 1.0 * np.sum(out.grad, ay).reshape(other.shape)
+
+        out._backward = _backward
+        return out
 
     def __mul__(self, other):
         return Tensor(self.data * other.data)
@@ -27,44 +55,50 @@ class Tensor:
     def __sub__(self, other):
         return Tensor(self.data - other.data)
 
-    def __truediv__(self, other):
-        return Tensor(self.data / other.data)
-
-    def __pow__(self, other):
-        # TODO: add scalar support
-        return Tensor(self.data**other.data)
-
     def __matmul__(self, other):
-        out = Tensor(self.data @ other.data)
+        out = Tensor(self.data @ other.data, _children=(self, other))
 
         def _backward():
-            self.grad = out.grad @ other.data.T
-            other.grad = self.data.T @ out.grad
-            assert self.grad.shape == self.data.shape
-            assert other.grad.shape == other.data.shape
+            self.grad += out.grad @ other.data.T
+            other.grad += self.data.T @ out.grad
 
         out._backward = _backward
         return out
-
-    @property
-    def requires_grad(self):
-        return self._requires_grad
-
-    @requires_grad.setter
-    def requires_grad(self, value):
-        if value and not self._requires_grad:
-            self._requires_grad = value
-            self.grad = np.ones_like(self.data)
 
     def softmax(self, ys):
         exps = np.exp(self.data - np.max(self.data, axis=1, keepdims=True))
         probs = exps / np.sum(exps, axis=1, keepdims=True)
+        out = Tensor(probs, _children=(self,))
 
         def _backward():
-            self.grad = (
-                probs - np.eye(probs.shape[1], dtype=np.float32)[ys]
-            ) / probs.shape[0]
+            self.grad += (
+                (probs - np.eye(probs.shape[1], dtype=np.float32)[ys]) / probs.shape[0]
+            ) * out.grad
 
-        out = Tensor(probs)
         out._backward = _backward
         return out
+
+    def relu(self):
+        out = Tensor(np.maximum(0, self.data), _children=(self,))
+
+        def _backward():
+            self.grad += (out.data > 0).astype(np.float32) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+        self.grad = np.ones_like(self.data, dtype=np.float32)
+        for node in reversed(topo):
+            node._backward()
