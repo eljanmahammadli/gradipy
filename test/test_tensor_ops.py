ae340fdb7e0cde7ae9f2c8af11f973bea5a377b9
Eljan Mahammadli <eljanmahammadli@gmail.com>
2023-11-02 15:36:41 -0400

    added addition and relu test

--- test/test_tensor_ops.py
+++ test/test_tensor_ops.py
@@ -49,3 +49,48 @@ def test_matmul():
     np.testing.assert_allclose(rgp, rpt, atol=1e-5)
     np.testing.assert_allclose(aggp, agpt, atol=1e-5)
     np.testing.assert_allclose(bggp, bgpt, atol=1e-5)
+
+
+def test_addition():
+    # create data
+    a = np.random.randn(5, 3, 4, 1).astype(np.float32)
+    b = np.random.randn(3, 1, 10).astype(np.float32)
+    # pytorch
+    apt = torch.tensor(a, requires_grad=True, dtype=torch.float32)
+    bpt = torch.tensor(b, requires_grad=True, dtype=torch.float32)
+    addpt = apt + bpt
+    addpt.backward(gradient=torch.ones_like(addpt, dtype=torch.float32))
+    rpt, agpt, bgpt = addpt.data.numpy(), apt.grad.numpy(), bpt.grad.numpy()
+    # gradipy
+    agp = Tensor(a)
+    bgp = Tensor(b)
+    addgp = agp + bgp
+    addgp.backward()
+    rgp, aggp, bggp = addgp.data, agp.grad, bgp.grad
+    # test
+    np.testing.assert_allclose(rgp, rpt)
+    np.testing.assert_allclose(aggp, agpt)
+    np.testing.assert_allclose(bggp, bgpt)
+
+
+def test_matmul_relu():
+    # create data
+    m, n, h = 15, 20, 30
+    a = np.random.randn(m, n).astype(np.float32)
+    b = np.random.randn(n, h).astype(np.float32)
+    # pytorch
+    apt = torch.tensor(a, requires_grad=True)
+    bpt = torch.tensor(b, requires_grad=True)
+    relupt = F.relu(apt @ bpt)
+    relupt.backward(gradient=torch.ones_like(relupt, dtype=torch.float32))
+    rpt, agpt, bgpt = relupt.data.numpy(), apt.grad.numpy(), bpt.grad.numpy()
+    # gradipy
+    agp = Tensor(a)
+    bgp = Tensor(b)
+    relugp = (agp @ bgp).relu()
+    relugp.backward()
+    rgp, aggp, bggp = relugp.data, agp.grad, bgp.grad
+    # compare
+    np.testing.assert_allclose(rgp, rpt, atol=1e-5)
+    np.testing.assert_allclose(aggp, agpt, atol=1e-5)
+    np.testing.assert_allclose(bggp, bgpt, atol=1e-5)
