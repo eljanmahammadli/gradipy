20e4c286b8223c6f36b0600f25c4319318049de8
Eljan Mahammadli <eljanmahammadli@gmail.com>
2023-11-05 02:06:36 -0500

    support for more init methods, uniform and xavier

--- gradipy/nn/__init__.py
+++ gradipy/nn/__init__.py
@@ -1,3 +1,8 @@
 from .loss import CrossEntropyLoss
-from .init import init_kaiming_normal
+from .init import (
+    init_kaiming_normal,
+    init_xavier_normal,
+    init_kaiming_uniform,
+    init_xavier_uniform,
+)
 from . import optim
--- gradipy/nn/init.py
+++ gradipy/nn/init.py
@@ -2,10 +2,33 @@ import math
 import numpy as np
 from gradipy.tensor import Tensor
 
+gain = {"relu": math.sqrt(2), "sigmoid": 1, "tanh": 5 / 3}
 
+
+# default fan_mode is fan_in for all the initialization methods
 def init_kaiming_normal(*args, nonlinearity="relu"):
-    gain = {"relu": math.sqrt(2), "sigmoid": 1, "tanh": 5 / 3}[nonlinearity]
-    fan = math.sqrt((args[0]))  # fan_in
-    std = gain / fan
-    normal = (np.random.randn(*args) * std).astype(np.float32)
-    return Tensor(normal)
+    fan_mode = args[0]
+    std = gain[nonlinearity] / math.sqrt(fan_mode)
+    dist = (np.random.randn(*args) * std).astype(np.float32)
+    return Tensor(dist)
+
+
+def init_kaiming_uniform(*args, nonlinearity="relu"):
+    fan_mode = args[0]
+    bound = gain[nonlinearity] * math.sqrt(3.0 / fan_mode)
+    dist = np.random.uniform(-bound, bound, size=(args)).astype(np.float32)
+    return Tensor(dist)
+
+
+def init_xavier_normal(*args, nonlinearity="sigmoid"):
+    fan_in, fan_out = args[0], args[-1]
+    std = gain[nonlinearity] * math.sqrt(2.0 / (fan_in + fan_out))
+    dist = (np.random.randn(*args) * std).astype(np.float32)
+    return Tensor(dist)
+
+
+def init_xavier_uniform(*args, nonlinearity="sigmoid"):
+    fan_in, fan_out = args[0], args[-1]
+    bound = gain[nonlinearity] * math.sqrt(6.0 / (fan_in + fan_out))
+    dist = np.random.uniform(-bound, bound, size=(args)).astype(np.float32)
+    return Tensor(dist)
